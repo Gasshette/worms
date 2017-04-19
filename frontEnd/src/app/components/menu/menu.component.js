5f1fee69d1dec90b3c3dbac6921ebcf2a8a39db2
Gasshette <gesshokun@gmail.com>
2017-04-19 17:37:59 +0200

    blabla adrien stuff

--- frontEnd/src/app/components/menu/menu.component.js
+++ frontEnd/src/app/components/menu/menu.component.js
@@ -1,7 +1,19 @@
 {
     "use strict";
     class MenuController {
+
         constructor() {
+            this.style = {
+                borderBottom: '#fff'
+            };
+        }
+
+        changeColor() {
+            this.style.borderBottom = "2px solid"  + '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
+
+        resetColor(){
+            this.style.borderBottom = "2px solid #fff";
         }
     }
 
@@ -11,6 +23,7 @@
         "menuComponent",
         {
             controller: MenuController,
+            controllerAs: "mc",
             templateUrl: "app/components/menu/menu.component.html"
         });
 }
